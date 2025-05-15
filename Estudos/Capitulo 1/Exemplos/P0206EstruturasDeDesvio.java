638e3a2c0aa3f86a3760fddeebafd13cea2114a5
Gabriela Garcia <114267576+Nicagab@users.noreply.github.com>
2025-05-15 18:49:16 -0300

    Add files via upload

---
+++ Estudos/Capitulo 1/Exemplos/P0206EstruturasDeDesvio.java
@@ -0,0 +1,26 @@
+public class P0206EstruturasDeDesvio {
+    public static void main(String[] args) {
+        int a = 25;
+        int b = 20;
+
+        // Diretiva If else
+        if (a>b) {
+            System.out.println("Verdadeiro");
+        } else if (a<b){
+            System.out.println("Verdadeiro");
+        } else {
+            System.out.println("Falso");
+        }
+
+         // Diretiva Switch
+
+        switch (a) {
+            case 1:
+                System.out.println("Caso 1");
+                break;
+            default:
+                System.out.println("Caso default");
+
+    }
+    }
+}
